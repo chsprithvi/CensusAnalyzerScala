4e46104986c47c13c84b9fc8bc401ad23b54b749
chsprithvi <chsprithvi@gmail.com>
2020-10-13 15:38:03 +0530

    [Siddhartha]Added: Function to load Indian CSV file

--- src/main/scala/CensusAnalyzer.scala
+++ src/main/scala/CensusAnalyzer.scala
@@ -30,4 +30,31 @@ class CensusAnalyzer {
       case _: FileNotFoundException => throw new CensusAnalyzerException(CustomException.wrongFilePath)
     }
   }
+  def loadIndiaStateCode(filePath:String):Int={
+    try {
+      if(!filePath.endsWith(".csv")){
+        throw new CensusAnalyzerException(CustomException.wrongFileType)
+      }
+      val fileReader = Source.fromFile(filePath)
+      var countRow = 0
+      for (line <- fileReader.getLines()) {
+        val cols = line.split(",").map(_.trim)
+
+        if (cols.length != 4){
+          throw new CensusAnalyzerException(CustomException.wrongDelimiter)
+        }
+
+        if (countRow == 0){
+          if(cols(0) != "SrNo" || cols(1) != "State Name" || cols(2) != "TIN" || cols(3) != "StateCode"){
+            throw new CensusAnalyzerException(CustomException.wrongHeaders)
+          }
+        }
+        countRow += 1
+      }
+      countRow - 1
+    }
+    catch {
+      case _: FileNotFoundException => throw new CensusAnalyzerException(CustomException.wrongFilePath)
+    }
+  }
 }
--- src/test/scala/CensusAnalyzerTest.scala
+++ src/test/scala/CensusAnalyzerTest.scala
@@ -2,44 +2,53 @@ import org.scalatest.FunSuite
 class CensusAnalyzerTest extends FunSuite {
   val CorrectFilePath   = "./src/test/resources/IndiaStateCensusData.csv"
   val WrongFilePath  = "./src/resources/IndiaStateCensusData.csv"
-  val WrongFileType  = "./src/test/resources/IndiaStateCensusData.txt"
+  val WrongFileType  = "./src/test/resources/IndiaStateData.txt"
   val InvalidDelimiterFile = "./src/test/resources/InvalidDelimitersIndiaStateCensusData.csv"
   val InvalidHeaderFile = "./src/test/resources/InvalidHeadersIndiaStateCensusData.csv"
-
+  val correctIndianStateFile = "./src/test/resources/IndiaStateCode.csv"
+  val censusAnalyser = new CensusAnalyzer()
+  val InvalidDelimiterStateFile="./src/test/resources/InvalidDelimiterIndiaStateCode.csv"
   test("given_IndianCensusCSVFile_ShouldReturnCorrectRecords") {
-    val objCensus = new CensusAnalyzer()
-    assert(objCensus.loadIndiaCensusData(CorrectFilePath) == 29)
+    assert(censusAnalyser.loadIndiaCensusData(CorrectFilePath) == 29)
   }
 
   test("givenIndianCensusDataCSVFile_whenWithWrongFilePath_ShouldThrowException") {
     val throws = intercept[Exception] {
-      val objCensus = new CensusAnalyzer()
-      objCensus.loadIndiaCensusData(WrongFilePath)
+      censusAnalyser.loadIndiaCensusData(WrongFilePath)
     }
     assert(throws.getMessage == CustomException.wrongFilePath.toString)
   }
 
-  test("givenIndianCensusDataCSVFile_whenWithWrongFileType_ShouldThrowException"){
-    val throws= intercept[Exception]{
-      val objCensus = new CensusAnalyzer
-      objCensus.loadIndiaCensusData(WrongFileType)
-    }
-    assert(throws.getMessage == CustomException.wrongFileType.toString)
-  }
-
   test("givenIndianCensusDataCSVFile_whenWithWrongDelimiters_shouldThrowException"){
     val throws = intercept[Exception] {
-      val objCensus = new CensusAnalyzer
-      objCensus.loadIndiaCensusData(InvalidDelimiterFile)
+      censusAnalyser.loadIndiaCensusData(InvalidDelimiterFile)
     }
     assert(throws.getMessage == CustomException.wrongDelimiter.toString)
   }
 
   test("givenIndianCensusDataCSVFile_WhenWithWrongHeaders_ShouldThrowException"){
     val throws = intercept[Exception]{
-      val objCensus = new CensusAnalyzer
-      objCensus.loadIndiaCensusData(InvalidHeaderFile)
+      censusAnalyser.loadIndiaCensusData(InvalidHeaderFile)
     }
     assert(throws.getMessage == CustomException.wrongHeaders.toString)
+  }
+  //For Indian State Data . CSV
+  test("givenIndiaStateCodeCSVFile_ShouldReturn_CorrectRecords") {
+    assert(censusAnalyser.loadIndiaStateCode(correctIndianStateFile) == 37)
+  }
+
+  test("givenIndianDataCSVFile_whenWithWrongFileType_ShouldThrowException"){
+    val throws= intercept[Exception]{
+      censusAnalyser.loadIndiaStateCode(WrongFileType)
+    }
+    assert(throws.getMessage == CustomException.wrongFileType.toString)
+  }
+
+
+  test("givenIndiaStateDataCSVFile_whenWithWrongDelimiters_shouldThrowException"){
+    val throws = intercept[Exception] {
+      censusAnalyser.loadIndiaStateCode(InvalidDelimiterStateFile)
+    }
+    assert(throws.getMessage == CustomException.wrongDelimiter.toString)
   }
 }
